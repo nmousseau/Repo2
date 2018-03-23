5608a7836c727122020fd12d65a6d8b4f069cbe9
nmousseau <36670797+nmousseau@users.noreply.github.com>
2018-03-23 11:42:46 -0400

    Add files via upload
    Fixed print code

--- ACGT.py
+++ ACGT.py
@@ -3,6 +3,7 @@ count_A = 0
 count_C = 0
 count_G = 0
 count_T = 0
+add = 0
 for letter in mystring:
     if letter == 'A':
         count_A += 1
@@ -12,29 +13,27 @@ for letter in mystring:
         count_G += 1
     if letter == 'T':
         count_T += 1
+    add += 1
 
-add = count_A + count_C + count_G + count_T
-print(add)
+#add = count_A + count_C + count_G + count_T
+print(str(add) + " Total entries")
+#print(add + " Total entries")
 
 divide = count_A / add
 multiply = divide * 100
-print("the % of A's is")
-print(multiply)
+print(str(multiply) + " % are A's")
 
 divide = count_C / add
 multiply = divide * 100
-print("the % of C's is")
-print(multiply)
+print(str(multiply) + " % are C's")
 
 divide = count_G / add
 multiply = divide * 100
-print("the % of G's is")
-print(multiply)
+print(str(multiply) + " % are G's")
 
 divide = count_T / add
 multiply = divide * 100
-print("the % of T's is")
-print(multiply)
+print(str(multiply) + " % are T's")
 
 
 
